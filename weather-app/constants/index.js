0c913498915311c98ac41e83df667fae26b9e84b
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-07-05 17:20:32 +0300

    final version

--- weather-app/constants/index.js
+++ weather-app/constants/index.js
@@ -1,11 +1,13 @@
-import { apiKey } from '@env';
-
-export const apiKey = apiKey;
+export const apiKey = '159ef38adbc14f80917114446230507';
 
 export const weatherImages = {
   'Partly cloudy': require('../assets/images/partlycloudy.png'),
   'Moderate rain': require('../assets/images/moderaterain.png'),
   'Patchy rain possible': require('../assets/images/moderaterain.png'),
+  Sunny: require('../assets/images/sun.png'),
+  Clear: require('../assets/images/sun.png'),
+  Overcast: require('../assets/images/cloud.png'),
+  Cloudy: require('../assets/images/cloud.png'),
   'Light rain': require('../assets/images/moderaterain.png'),
   'Moderate rain at times': require('../assets/images/moderaterain.png'),
   'Heavy rain': require('../assets/images/heavyrain.png'),
@@ -15,8 +17,4 @@ export const weatherImages = {
   'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
   Mist: require('../assets/images/mist.png'),
   other: require('../assets/images/moderaterain.png'),
-  Sunny: require('../assets/images/sun.png'),
-  Clear: require('../assets/images/sun.png'),
-  Overcast: require('../assets/images/cloud.png'),
-  Cloudy: require('../assets/images/cloud.png'),
 };
